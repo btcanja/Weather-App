e360d782454999219778fbe4b61cc53a736238a0
btcanja <116056586+btcanja@users.noreply.github.com>
2022-10-31 22:50:33 +0800

    Update forApi.js

--- src/forApi.js
+++ src/forApi.js
@@ -1,4 +1,4 @@
-const geoApiKey = "c51b949df2af4b84b81f0788ba5701c5";
+const geoApiKey = "{Insert Geo API Key here}";
 
 export const geoApiUrlPrefix =
   "https://api.geoapify.com/v1/geocode/search?text=";
@@ -6,4 +6,4 @@ export const geoApiUrlPrefix =
 export const geoApiUrlSuffix = `&lang=en&type=city&apiKey=${geoApiKey}`;
 
 export const weatherApiUrlPrefix = "https://api.openweathermap.org/data/2.5";
-export const weatherApiKey = "5639f2fc12c2ef627eaa0b4d663c186c";
+export const weatherApiKey = "{Insert Openweathermap API key here}";
